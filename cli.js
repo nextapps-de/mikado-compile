c816b4b27e7236f4f5e4c9b7832aac61eac563f0
Daniel Sokil <dtechtours@gmail.com>
2019-11-07 19:11:50 -0500

    Added File Watcher

--- cli.js
+++ cli.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
+const path = require("path");
 const parse_argv = require("mri");
+const list_files = require("totalist/sync");
+
 const compile = require("./compile.js");
 
 const argv = process.argv.slice(2);
@@ -10,7 +14,8 @@ const options = parse_argv(argv, {
     d: "dest",
     t: "type",
     f: "force",
-    p: "pretty"
+    p: "pretty",
+    w: "watch"
   },
   default: {
     force: true,
@@ -21,14 +26,28 @@ const options = parse_argv(argv, {
 const src = options._[0] || options.src;
 const dest = options._[1] || options.dest;
 const type = options._[2] || options.type;
-const { force, pretty } = options;
+const { force, pretty, watch } = options;
 
-console.log(src, dest, type, force, pretty);
-
-if (src) {
+const compiler = src =>
   compile(src, dest, {
     type,
     force,
     pretty
   });
-}
+
+const watcher = src => {
+  console.info(`Watching: ${src}`);
+  fs.watch(src, (eventType, _) => {
+    if (eventType === "change") compiler(src);
+  });
+};
+
+if (watch) {
+  const srcStats = fs.statSync(src);
+  if (srcStats.isDirectory()) {
+    list_files(src, name => {
+      const relPath = path.join(src, name);
+      watcher(relPath);
+    });
+  } else watcher(src);
+} else if (src) compiler(src);
